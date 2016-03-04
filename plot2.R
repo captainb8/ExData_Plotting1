d16cb1ebbf97536db2159bfae71f4caa08588f58
captainb8 <brownljt2@gmail.com>
2016-03-04 11:38:15 -0500

    Added files via upload

---
+++ plot2.R
@@ -0,0 +1,26 @@
+#set working directory
+setwd("C:/Users/jbrownlee/Google Drive/Coursera/04_exploratory_data_analysis/course project 1")
+
+#download data
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destFile = "exdata-data-household_power_consumption.zip"
+download.file(fileURL, destFile)
+
+#unzip file
+unzip(destFile)
+
+#load data
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+
+#subset the data
+data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+data$datetime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+#create plot on screen device
+plot(data$datetime, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+#copy to PNG
+dev.copy(png, file="plot2.png", width=480, height=480)
+dev.off()
+
